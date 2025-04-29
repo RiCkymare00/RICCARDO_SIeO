6bea09c5a3da89d7cbb9c5ece7f7cd34b169fc4a
RiCkymare00 <maregariccardo.rm0@gmail.com>
2025-04-29 13:14:52 +0200

    Rimosso la cartella BELLANTI dal repository

--- BELLANTI/ConsoleAppTemp/V2.0/ConsoleAppTemp/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ConsoleAppTemp;
-using System.Configuration;
-
-Console.WriteLine("Esecuzione programma in corso...");
-Run r = new Run();
-string tipo= ConfigurationManager.AppSettings["Tipo"].ToString();
-r.ExecProgram(tipo);
